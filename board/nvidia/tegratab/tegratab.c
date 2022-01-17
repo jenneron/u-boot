7614e6c1f0b0cbf4cc1c008fefa7400c5bd8c982
Anton Bambura <jenneron@protonmail.com>
2022-01-17 21:25:02 +0200

    wip: add support for tegratab

---
+++ board/nvidia/tegratab/tegratab.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <common.h>
+#include <dm.h>
+#include <log.h>
+#include <asm/arch/pinmux.h>
+#include <asm/arch/gp_padctrl.h>
+#include "pinmux-config-tegratab.h"
+#include <i2c.h>
+
+/* empty for now */
---
+++ include/configs/tegratab.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <linux/sizes.h>
+
+#include "tegra114-common.h"
+
+/* High-level configuration options */
+#define CONFIG_TEGRA_BOARD_STRING	"NVIDIA Tegra Note 7"
+
+/* Board-specific serial config */
+#define CONFIG_TEGRA_ENABLE_UARTD
+#define CONFIG_SYS_NS16550_COM1		NV_PA_APB_UARTD_BASE
+
+/* Environment in eMMC, at the end of 2nd "boot sector" */
+
+#include "tegra-common-usb-gadget.h"
+#include "tegra-common-post.h"
+
+#endif /* __CONFIG_H */
